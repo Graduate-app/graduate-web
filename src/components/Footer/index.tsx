9daa5da12dfb8bef9b323db2ca46865c5a2ebf00
driikoK <kupchak.a.m@gmail.com>
2025-06-27 09:57:18 +0300

    Small adjustments

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <footer className="bg-gray-50 py-4">
       <div className="container mx-auto px-6 text-center lg:px-16">
         <p className="text-sm text-gray-500">
-          Прикарпатський національний університет імені Василя Стефаника - {new Date().getFullYear()} р.
+          Карпатський національний університет імені Василя Стефаника - {new Date().getFullYear()} р.
         </p>
       </div>
     </footer>
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,13 +3,12 @@ const Header = () => {
     <header className="bg-white py-4 px-6 shadow-sm lg:px-16">
       <div className="container mx-auto flex flex-col gap-3 md:flex-row md:items-center md:justify-between">
         <div className="flex items-center gap-4">
-          <img src="/logo.png" alt="logo" className="h-12 w-12" />
           <div>
             <h2 className="text-lg font-medium text-gray-800">
               Кафедра комп'ютерної інженерії та електроніки
             </h2>
             <p className="text-sm text-gray-600">
-              Прикарпатський національний університет імені Василя Стефаника
+              Карпатський національний університет імені Василя Стефаника
             </p>
           </div>
         </div>
